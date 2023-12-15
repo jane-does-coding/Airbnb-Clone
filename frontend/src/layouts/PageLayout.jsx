386a0027fe9613f43963ac2f73d4ae1f11d6b318
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2023-12-14 19:39:33 -0700

    button component done

--- frontend/src/layouts/PageLayout.jsx
+++ frontend/src/layouts/PageLayout.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import Navbar from "../components/Navbar/Navbar";
+import Modal from "../components/Modals/Modal";
 
 const PageLayout = ({ children }) => {
   return (
     <div>
+      <Modal isOpen={true} title={"Hello World"} />
       <Navbar /> {children}
     </div>
   );
