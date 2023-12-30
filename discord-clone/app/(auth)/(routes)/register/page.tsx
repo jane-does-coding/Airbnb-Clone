db12f7fc7682d2cbbf77a1d9ce8e1ccd5af96e35
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2023-12-30 10:06:10 -0700

    folder structure done

---
+++ discord-clone/app/(auth)/(routes)/register/page.tsx
@@ -0,0 +1,5 @@
+const RegisterPage = () => {
+  return ( <div>register</div> );
+}
+ 
+export default RegisterPage;
---
+++ discord-clone/app/(main)/(routes)/page.tsx
@@ -0,0 +1,10 @@
+
+
+export default function Home() {
+  return (
+    <div className=''>
+     This is a protected route
+    </div>
+
+  )
+}
