7a31c1c45e4ab22c4a2c560d1b73c73ea3c7e512
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2023-12-30 13:04:03 -0700

    darm light switch theme done

--- discord-clone/app/(main)/(routes)/page.tsx
+++ discord-clone/app/(main)/(routes)/page.tsx
@@ -1,3 +1,4 @@
+import { ModeToggle } from "@/components/mode-toggle";
 import { UserButton } from "@clerk/nextjs";
 
 
@@ -5,6 +6,7 @@ export default function Home() {
   return (
     <div className=''>
      <UserButton afterSignOutUrl="/sign-in" />
+     <ModeToggle />
     </div>
 
   )
--- discord-clone/app/layout.tsx
+++ discord-clone/app/layout.tsx
@@ -3,6 +3,7 @@ import { Open_Sans } from 'next/font/google'
 import './globals.css'
 import {ClerkProvider} from "@clerk/nextjs"
 import { ThemeProvider } from '@/components/providers/theme-provider'
+import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
 
@@ -18,8 +19,8 @@ export default function RootLayout({
 }) {
   return (
     <ClerkProvider>
-      <html lang="en">
-        <body className={font.className}>
+      <html lang="en" suppressHydrationWarning>
+        <body className={cn(font.className, "bg-white dark:bg-[#313338]") }>
           <ThemeProvider attribute='class' defaultTheme='dark' enableSystem={false} storageKey='discord-theme'>
             {children}
           </ThemeProvider>
