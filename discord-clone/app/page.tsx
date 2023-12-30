4715b659491159cb80a21e3ceffdb80e1a3f24f6
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2023-12-29 21:54:31 -0700

    clean up and setup

--- discord-clone/app/page.tsx
+++ discord-clone/app/page.tsx
@@ -1,7 +1,12 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
   return (
-   <p>hello world</p>
+    <div className=''>
+      <p className='text-5xl text-red-600'>hello world</p>
+      <Button>click me</Button>
+    </div>
+
   )
 }
