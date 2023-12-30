f52ea5ad6295d75fe1978db7323b7dcf7b64d6e9
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2023-12-29 21:02:57 -0700

    its discord now

--- backend/routes/userRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { signupUser } = require("../controllers/userController");
-
-router.post("/signup", signupUser);
-
-module.exports = router;
