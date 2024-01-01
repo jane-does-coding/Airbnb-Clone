72d989554bf605d7bb8eee75ec554dde5294602b
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-01-01 16:02:14 -0700

    social logins don't work but at least the signup/login work

--- airbnb-clone/app/actions/GetCurrentUser.ts
+++ airbnb-clone/app/actions/GetCurrentUser.ts
@@ -1,6 +1,7 @@
-import { getServerSession } from "next-auth";
+import { getServerSession } from "next-auth/next"
+
 import { authOptions } from "@/pages/api/auth/[...nextauth]";
-import prisma from "@/app/libs/prismadb"
+import prisma from "@/app/libs/prismadb";
 
 export async function getSession() {
   return await getServerSession(authOptions)
@@ -8,29 +9,30 @@ export async function getSession() {
 
 export default async function getCurrentUser() {
   try {
-    const session = await getSession()
+    const session = await getSession();
 
-    if(!session?.user?.email){
-      return null
+    if (!session?.user?.email) {
+      return null;
     }
 
     const currentUser = await prisma.user.findUnique({
       where: {
-        email: session.user.email as string
+        email: session.user.email as string,
       }
-    })
+    });
 
-    if(!currentUser){
-      return null
+    if (!currentUser) {
+      return null;
     }
 
     return {
       ...currentUser,
       createdAt: currentUser.createdAt.toISOString(),
       updatedAt: currentUser.updatedAt.toISOString(),
-      emailVerified: currentUser.emailVerified?.toISOString() || null
-    }
+      emailVerified: 
+        currentUser.emailVerified?.toISOString() || null,
+    };
   } catch (error: any) {
-    return null
+    return null;
   }
 }
