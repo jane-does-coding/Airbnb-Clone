52b91b8f62846179b8ea6a6b03cfe57c34982fda
Yevheniia Simaka <yevheniiasimaka@gmail.com>
2024-01-07 12:46:57 -0700

    done

--- airbnb-clone/app/page.tsx
+++ airbnb-clone/app/page.tsx
@@ -24,9 +24,7 @@ const Home = async({searchParams}: HomeProps) => {
       <div className="pt-20 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6 gap-4 2xl:gap-8">
         {listings.map((listing) => {
           return (
-            <div className="">
-              <ListingCard currentUser={currentUser} key={listing.id} data={listing} />
-            </div>
+            <ListingCard currentUser={currentUser} key={listing.id} data={listing} />
           )
         })}
       </div>
